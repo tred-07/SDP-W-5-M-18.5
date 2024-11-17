328d3ac759b1ab8e6fe7c14c776c6ba142e4b346
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-11-17 17:02:20 +0600

    Done

--- project/app/form.py
+++ project/app/form.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 from django.contrib.auth.models import User
 from django import forms
 
@@ -10,3 +10,13 @@ class SignUp(UserCreationForm):
     class Meta:
         model=User
         fields=['username','first_name','last_name','email']
+
+
+
+class EditForm(UserChangeForm):
+    first_name=forms.CharField(widget=forms.TextInput(attrs={'id':'required','placeholder':'First Name'}))
+    last_name=forms.CharField(widget=forms.TextInput(attrs={'id':'required','placeholder':'Last Name'}))
+    password=None
+    class Meta:
+        model=User
+        fields=['first_name','last_name','email']
--- project/app/urls.py
+++ project/app/urls.py
@@ -2,5 +2,12 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.signup,name='signup')
+    path('',views.home,name='home'),
+    path('profile/',views.profile,name='profile'),
+    path('signup/',views.signup,name='signup'),
+    path('login/',views.logIn,name='login'),
+    path('logout/',views.logOut,name='logout'),
+    path('change_password/',views.change_password,name='change_password')
+    ,path('change_password_without_old_password/',views.set_password,name='Change_Password_Without_Old_Password'),
+    path('edit_profile/',views.edit_profile,name='edit_profile')
 ]
